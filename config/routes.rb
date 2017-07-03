5c88a62c7468892b3f09ab3c81f318084d3d0f52
Tranducthang28011996 <tieuhodo96@gmail.com>
2017-07-03 10:36:07 +0700

    login with facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   get 'hocmachoi' => 'hocmachoi#index'
   get 'kiemtra' => 'kiemtra#index'
-  devise_for :users
+  # devise_for :users
   root 'home#index'
   get 'home' => 'home#index'
   get 'thidau' => 'thidau#index'
   resources :rooms
   get 'bangxephang' => 'bangxephang#index'
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
